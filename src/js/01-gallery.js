63d4708d676749b6714b39b9bf0da563789f05fe
VladyslavKosarenko <131978971+VladyslavKosarenko@users.noreply.github.com>
2023-09-11 00:33:12 +0300

    finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+function partsGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+        `;
+        
+    }).join('');
+}
+
+const ul = document.querySelector('.gallery');
+const markup = partsGallery(galleryItems);
+
+ul.insertAdjacentHTML('beforeend', markup);
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    sourceAttr: 'href', 
+    captions: true,
+    captionDelay: 250,
+    captionPosition: 'bottom',
+    captionType: 'attr',
+    captionsData: 'alt', 
+    
+});
+    
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+function saveCurrentTime(time) {
+    localStorage.setItem('videoplayer-current-time', time);
+}
+player.on('timeupdate', (data) => {
+    const currentTime = data.seconds;
+    saveCurrentTime(currentTime);
+});
+function restoreCurrentTime() {
+    const savedTime = localStorage.getItem('videoplayer-current-time');
+    if (savedTime) {
+        player.setCurrentTime(savedTime).catch((error) => {
+            console.error('Помилка при відновленні часу відтворення:', error);
+        });
+    }
+}
 
+
+restoreCurrentTime();
+
+const throttledSaveTime = throttle(saveCurrentTime, 1000);
+
+
+player.on('timeupdate', (data) => {
+    const currentTime = data.seconds;
+    throttledSaveTime(currentTime);
+});
